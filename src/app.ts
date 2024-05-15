28a73d80ad6765450309eb04a4b453174b3d30bd
Talha <mdtalha2008@gmail.com>
2024-05-16 00:34:40 +0600

    basic project setup done

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+
+//parms
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+  res.send('Hello World!');
+});
+
+export default app;
+
+// console.log(process.cwd());
+//D:\Programming-Hero-Level-2\first-project/.env
